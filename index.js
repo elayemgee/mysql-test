f23b0e5630c7cf0f92f7b22da05756ce85060a22
Sophia <sophia.oquias@gmail.com>
2023-04-07 15:08:45 +0800

    changed configs again

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(express.static('public'));
 
 const mysql = require('mysql2');
 var con = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
+    host: 'ccscloud3.dlsu.edu.ph',
+    port: '39000',
     user: 'dev',
     password: '12341234'
 });
