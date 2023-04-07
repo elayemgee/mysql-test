6fefbe3b32ab9d7bb7044cf53ecb71ecd2cd00f0
Sophia <sophia.oquias@gmail.com>
2023-04-07 14:15:41 +0800

    connected to mysql db

--- index.js
+++ index.js
@@ -16,11 +16,35 @@ app.engine("hbs", exphbs.engine({
 
 app.use(express.static('public'));
 
+const mysql = require('mysql2');
+var con = mysql.createConnection({
+    host: 'ccscloud3.dlsu.edu.ph',
+    port: '39000',
+    user: 'dev',
+    password: '12341234'
+});
+
+con.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + con.threadId);
+});
+
 app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, '/index.html'));
-    res.render('index');
+    //   res.sendFile(path.join(__dirname, '/index.html'));
+    var query = "SELECT * FROM imdb.movies LIMIT 10;"
+    con.query(query, function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        // connected!
+
+        res.render('index', { tuple: results });
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
